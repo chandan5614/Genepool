c04cc4b6b403197006f059229d3b10f4ec74b62d
chandan5614 <chandan.dy1995@gmail.com>
2024-10-14 01:22:24 -0500

    Microservice Architecture with API Gateway Pattern - Structure

---
+++ Genepool/src/Architectures/MicroservicesArchitecture/ServiceA/Controllers/ServiceAController.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Genepool.src.Architectures.MicroservicesArchitecture.ServiceA.Controllers
+{
+    public class ServiceAController
+    {
+        
+    }
+}
