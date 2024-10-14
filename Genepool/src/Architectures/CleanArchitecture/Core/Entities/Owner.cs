8e5ffc30631fcadc3510d5974c807148c0d89690
chandan5614 <chandan.dy1995@gmail.com>
2024-10-13 21:40:55 -0500

    Define the Core Entitie

--- Genepool/src/Architectures/CleanArchitecture/Core/Entities/Owner.cs
+++ Genepool/src/Architectures/CleanArchitecture/Core/Entities/Owner.cs
@@ -1,12 +1,35 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
-namespace Genepool.src.Architectures.CleanArchitecture.Core.Entities
+namespace Genepool.Architectures.OnionArchitecture.Core.Entities
 {
     public class Owner
     {
-        
+        // Unique identifier for the owner
+        [Key]
+        public Guid Id { get; set; }
+
+        // Name of the owner
+        [Required(ErrorMessage = "Owner name is required.")]
+        [StringLength(100, ErrorMessage = "Owner name cannot be longer than 100 characters.")]
+        public string Name { get; set; }
+
+        // Email of the owner
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
+        public string Email { get; set; }
+
+        // Collection of vehicles owned by the owner
+        public ICollection<Vehicle> Vehicles { get; set; }
+
+        // Constructor
+        public Owner()
+        {
+            // Initialize the collection to prevent null reference exceptions
+            Vehicles = new List<Vehicle>();
+        }
+
+        // Additional business logic methods can be added here as needed
     }
 }
--- Genepool/src/Architectures/CleanArchitecture/Core/Entities/Vehicle.cs
+++ Genepool/src/Architectures/CleanArchitecture/Core/Entities/Vehicle.cs
@@ -1,12 +1,37 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Genepool.src.Architectures.CleanArchitecture.Core.Entities
+namespace Genepool.Architectures.OnionArchitecture.Core.Entities
 {
     public class Vehicle
     {
-        
+        // Unique identifier for the vehicle
+        [Key]
+        public Guid Id { get; set; }
+
+        // Make of the vehicle
+        [Required(ErrorMessage = "Vehicle make is required.")]
+        [StringLength(50, ErrorMessage = "Vehicle make cannot be longer than 50 characters.")]
+        public string Make { get; set; }
+
+        // Model of the vehicle
+        [Required(ErrorMessage = "Vehicle model is required.")]
+        [StringLength(50, ErrorMessage = "Vehicle model cannot be longer than 50 characters.")]
+        public string Model { get; set; }
+
+        // Year of manufacture
+        [Required(ErrorMessage = "Year is required.")]
+        [Range(1886, int.MaxValue, ErrorMessage = "Year must be greater than or equal to 1886.")]
+        public int Year { get; set; }
+
+        // Foreign key to link the vehicle to its owner
+        [ForeignKey("Owner")]
+        public Guid OwnerId { get; set; }
+
+        // Navigation property to the owner
+        public Owner Owner { get; set; }
+
+        // Additional business logic methods can be added here as needed
     }
 }
