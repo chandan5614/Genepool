2a9748fd797c7dd6b8b7f54ea704b68aa86f37cc
chandan5614 <chandan.dy1995@gmail.com>
2024-10-14 02:06:02 -0500

    Implement the controller for ServiceA

--- Genepool/src/Architectures/MicroservicesArchitecture/ServiceA/Controllers/OwnersController.cs
+++ Genepool/src/Architectures/MicroservicesArchitecture/ServiceA/Controllers/OwnersController.cs
@@ -1,12 +1,70 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+using Genepool.src.Architectures.MicroservicesArchitecture.ServiceA.Application.Dtos;
+using Genepool.src.Architectures.MicroservicesArchitecture.ServiceA.Application.Services;
+using Microsoft.AspNetCore.Mvc;
 
-namespace Genepool.src.Architectures.MicroservicesArchitecture.ServiceA.Controllers
+namespace Genepool.src.Architectures.MicroservicesArchitecture.ServiceA.Presentation.Controllers
 {
-    public class OwnersController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OwnersController : ControllerBase
     {
-        
+        private readonly OwnerService _ownerService;
+
+        public OwnersController(OwnerService ownerService)
+        {
+            _ownerService = ownerService;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetOwnerById(Guid id)
+        {
+            var owner = await _ownerService.GetOwnerByIdAsync(id);
+            if (owner == null)
+            {
+                return NotFound();
+            }
+            return Ok(owner);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllOwners()
+        {
+            var owners = await _ownerService.GetAllOwnersAsync();
+            return Ok(owners);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddOwner([FromBody] OwnerDto ownerDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            await _ownerService.AddOwnerAsync(ownerDto);
+            return CreatedAtAction(nameof(GetOwnerById), new { id = ownerDto.Id }, ownerDto);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateOwner(Guid id, [FromBody] OwnerDto ownerDto)
+        {
+            if (id != ownerDto.Id)
+            {
+                return BadRequest();
+            }
+
+            await _ownerService.UpdateOwnerAsync(ownerDto);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteOwner(Guid id)
+        {
+            await _ownerService.DeleteOwnerAsync(id);
+            return NoContent();
+        }
     }
 }
--- Genepool/src/Architectures/MicroservicesArchitecture/ServiceA/Controllers/VehiclesController.cs
+++ Genepool/src/Architectures/MicroservicesArchitecture/ServiceA/Controllers/VehiclesController.cs
@@ -1,12 +1,77 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+using Genepool.src.Architectures.MicroservicesArchitecture.ServiceA.Application.Dtos;
+using Genepool.src.Architectures.MicroservicesArchitecture.ServiceA.Application.Services;
+using Microsoft.AspNetCore.Mvc;
 
-namespace Genepool.src.Architectures.MicroservicesArchitecture.ServiceA.Controllers
+namespace Genepool.src.Architectures.MicroservicesArchitecture.ServiceA.Presentation.Controllers
 {
-    public class VehiclesController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class VehiclesController : ControllerBase
     {
-        
+        private readonly VehicleService _vehicleService;
+
+        public VehiclesController(VehicleService vehicleService)
+        {
+            _vehicleService = vehicleService;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetVehicleById(Guid id)
+        {
+            var vehicle = await _vehicleService.GetVehicleByIdAsync(id);
+            if (vehicle == null)
+            {
+                return NotFound();
+            }
+            return Ok(vehicle);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllVehicles()
+        {
+            var vehicles = await _vehicleService.GetAllVehiclesAsync();
+            return Ok(vehicles);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddVehicle([FromBody] VehicleDto vehicleDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            await _vehicleService.AddVehicleAsync(vehicleDto);
+            return CreatedAtAction(nameof(GetVehicleById), new { id = vehicleDto.Id }, vehicleDto);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateVehicle(Guid id, [FromBody] VehicleDto vehicleDto)
+        {
+            if (id != vehicleDto.Id)
+            {
+                return BadRequest();
+            }
+
+            await _vehicleService.UpdateVehicleAsync(vehicleDto);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteVehicle(Guid id)
+        {
+            await _vehicleService.DeleteVehicleAsync(id);
+            return NoContent();
+        }
+
+        [HttpGet("owner/{ownerId}")]
+        public async Task<IActionResult> GetVehiclesByOwnerId(Guid ownerId)
+        {
+            var vehicles = await _vehicleService.GetVehiclesByOwnerIdAsync(ownerId);
+            return Ok(vehicles);
+        }
     }
 }
